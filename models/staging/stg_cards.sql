c60f65c1d7f783516161208b8e93f1484608957c
tuan vo <votuan3719@gmail.com>
2025-09-10 20:46:17 +0000

    intermediate

--- models/staging/stg_cards.sql
+++ models/staging/stg_cards.sql
@@ -2,5 +2,6 @@ select
     json_data:id as card_id,
     json_data:name as name,
     json_data:supertype as supertype,
-    json_data:artist as artist
+    json_data:artist as artist,
+    json_data:images:large as img_url
 from {{ source("dbt_pokemon", "tcg_pokemon_api") }}
