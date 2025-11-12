6575af18776a15a6deb89bc6917eae3ff9a7f733
vobyt13@gmail.com <vobyt13@gmail.com>
2025-11-12 23:39:07 +0000

    update

--- models/intermediate/init_artists.sql
+++ models/intermediate/init_artists.sql
@@ -2,11 +2,11 @@ with artists as (
     select
         distinct artist
     from {{ ref("stg_cards") }}
+    where artist is not null
     order by artist
 )
 select
     row_number() over(order by artist) as artist_key,
     *
 from artists
-where artist is not null
 order by artist_key
