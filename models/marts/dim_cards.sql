a8a097e58d30f25cfcca68bd2af21aa42443c5cb
vobyt13@gmail.com <vobyt13@gmail.com>
2025-11-13 21:23:08 +0000

    update

--- models/marts/dim_cards.sql
+++ models/marts/dim_cards.sql
@@ -1,14 +1,50 @@
-select
-    c.card_key,
-    c.card_id,
-    c.card_name,
-    c.super_type,
-    c.img_url,
-    s.set_key,
-    a.artist_key
-from {{ ref("init_cards") }} as c
+with rarities as (
+    select
+        c.card_key,
+        c.card_id,
+        c.card_name,
+        c.set_id,
+        c.super_type,
+        r.card_rarity,
+        c.artist_name,
+        c.img_url,
+    from {{ ref("init_cards") }} as c
+    cross join {{ ref("init_rarities") }} as r
+),
+card_rarities as (
+    select distinct card_key, card_rarity
+    from {{ ref("init_card_prices") }}
+),
+cards as (
+    select 
+        row_number() over(order by cr.card_key, cr.card_rarity) as card_key,
+        r.card_id,
+        r.card_name,
+        r.set_id,
+        r.super_type,
+        r.artist_name,
+        r.img_url,
+        r.card_rarity
+    from card_rarities as cr
+    left join rarities as r
+        on cr.card_key = r.card_key
+        and cr.card_rarity = r.card_rarity
+)
+select 
+    c.card_key as card_key,
+    c.card_id as card_id,
+    c.card_name as card_name,
+    s.set_key as set_key,
+    c.super_type as super_type,
+    r.rarity_key as rarity_key,
+    a.artist_key as artist_key,
+    c.img_url as img_url
+from cards as c
 left join {{ ref("init_sets") }} as s
     on c.set_id = s.set_id
+left join {{ ref("init_rarities")}} as r
+    on c.card_rarity = r.card_rarity
 left join {{ ref("init_artists") }} as a
     on c.artist_name = a.artist_name
-order by card_key
+order by card_key, rarity_key
+
--- models/marts/fct_card_prices.sql
+++ models/marts/fct_card_prices.sql
@@ -1,15 +1,14 @@
-with
-    prices as (
+with prices as (
         select
             c.card_key::int as card_key,
             p.date::date as date,
             p.price_low::float as price_low,
             p.price_mid::float as price_mid,
             p.price_high::float as price_high,
-            p.market_price::float as price_market
+            p.price_market::float as price_market
         from {{ ref("stg_card_prices") }} as p
-        left join {{ ref("init_cards") }} as c using (card_id)
-        left join {{ ref("init_sets") }} as s using (set_id)
-        order by date, set_key, card_key
+        left join {{ ref("init_cards") }} as c 
+            on p.card_id = c.card_id
     )
 select * from prices
+order by card_key, date
